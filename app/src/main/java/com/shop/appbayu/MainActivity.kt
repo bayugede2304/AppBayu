3778c5391d73854f3dc0601bdad1043ec204806e
bayu <bayugede2304@gmail.com>
2023-07-11 20:14:01 +0700

    First init

---
+++ app/src/main/java/com/shop/appbayu/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shop.appbayu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
